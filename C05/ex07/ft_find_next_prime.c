cfd37d07bb4ef6e8d797a2c9192acba28776d4bb
dbobrov <dbobrov@student.42wolfsburg.de>
2025-09-15 19:23:04 +0200

    ssss

--- C05/ex07/ft_find_next_prime.c
+++ C05/ex07/ft_find_next_prime.c
@@ -3,23 +3,23 @@
 /*                                                        :::      ::::::::   */
 /*   ft_find_next_prime.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dbobrov <dbobrov@student.42wolfsburg.de    +#+  +:+       +#+        */
+/*   By: dbobrov <dbobrov@student.42wolfsburg.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/09/14 19:23:07 by dbobrov           #+#    #+#             */
-/*   Updated: 2025/09/14 19:23:07 by dbobrov          ###   ########.fr       */
+/*   Created: 2025/09/15 19:22:51 by dbobrov           #+#    #+#             */
+/*   Updated: 2025/09/15 19:22:51 by dbobrov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int ft_find_next_prime(int nb);
+int	ft_find_next_prime(int nb);
 
-int ft_is_prime(int nb)
+int	ft_is_prime(int nb)
 {
 	int	i;
 
 	if (nb < 2)
 		return (0);
 	i = 2;
-	while (i*i <= nb)
+	while (i * i <= nb)
 	{
 		if (nb % i == 0)
 			return (0);
@@ -28,7 +28,7 @@ int ft_is_prime(int nb)
 	return (1);
 }
 
-int ft_find_next_prime(int nb)
+int	ft_find_next_prime(int nb)
 {
 	while (1)
 	{
